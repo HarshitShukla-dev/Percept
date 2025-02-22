75f50758d2a1cc75a9b5d247be99ad4d94a93208
Harshit Shukla <hershitshukla35@gmail.com>
2025-02-23 00:36:41 +0530

    feat: update Docker configuration; create google-credentials.json file from environment variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import app from './app';
 
 dotenv.config();
 
+require('fs').writeFileSync('./credentials/google-credentials.json', process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
